3ab57646643157ed1902573c4c78cd00b08b16b8
LogeshSelvaraj <logeshksr99@gmail.com>
2020-11-25 18:07:26 +0530

    header about complete

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="row">
+      <div className="col col-md-4 col-centered">
+        <img src="/images/logo.png" alt="logo" width="100%" height="100%" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Components/Installations.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Installations() {
+  return (
+    <div></div>
+  );
+}
+
+export default Installations;
